472cca6cc79e5074918508686d3be00037ae8d84
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2025-02-23 19:02:17 +0200

    add user model and change AUTH_USER_MODEL

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     #new apps
     "airport",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -106,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+AUTH_USER_MODEL = "user.User"
 
 LANGUAGE_CODE = "en-us"
 
